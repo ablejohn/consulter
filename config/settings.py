6faf0d99819d89edc1f3fdedf6ac9e3579cc24a7
ABLE JOHN <100355902+ablejohn@users.noreply.github.com>
2024-05-24 16:24:26 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
