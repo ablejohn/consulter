bc0bb7fb5e7f41384094c955d53ca7d2a9721bf0
ABLE JOHN <100355902+ablejohn@users.noreply.github.com>
2024-05-24 15:17:50 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eozjspuo&4_d1vc=j1*o5-q978g5=h5i6vml$_p+mj25rxek9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
