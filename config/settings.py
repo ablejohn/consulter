7cf2657cca3454fe575895aa14cc13aa6183c5f0
ABLE JOHN <100355902+ablejohn@users.noreply.github.com>
2024-05-24 14:45:05 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eozjspuo&4_d1vc=j1*o5-q978g5=h5i6vml$_p+mj25rxek9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
